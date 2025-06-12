b2eff0bf9259905c9e34a21035d1054065cebac9
Le Hoang <lehoangnguyen510@gmail.com>
2025-06-12 21:09:49 +0200

    alexnet

---
+++ models/AlexNet/alexnet.py
@@ -0,0 +1,17 @@
+"""
+AlexNet archtecture implementation.
+Input size: 224x224x3 ()
+"""
+
+import torch
+import torch.nn as nn
+
+
+class AlexNet(nn.Module):
+    def __init__(self, num_classes=1000):
+        pass
+
+    def forward(self, x):
+        pass
+
+
