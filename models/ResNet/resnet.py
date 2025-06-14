cdaef34dd21f043ed9cee745528d4bf126a3c73c
Le Hoang <lehoangnguyen510@gmail.com>
2025-06-14 21:04:07 +0200

    finish resnet

--- models/ResNet/resnet.py
+++ models/ResNet/resnet.py
@@ -3,9 +3,9 @@ import torch.nn as nn
 import torch.nn.functional as F
 
 class BasicBlock(nn.Module):
-    expansion = 1 
+    expansion = 1
 
-    def __init__(self, in_channels, out_channels, stride, downsample=None):
+    def __init__(self, in_channels, out_channels, stride=1, downsample=None):
         super(BasicBlock, self).__init__()
 
         self.conv1 = nn.Conv2d(in_channels, out_channels, kernel_size=3, stride=stride, padding=1)
@@ -14,7 +14,7 @@ class BasicBlock(nn.Module):
         self.bn2 = nn.BatchNorm2d(out_channels)
 
         self.downsample = downsample
-    
+
     def forward(self, x):
         identity = x
         out = F.relu(self.bn1(self.conv1(x)))
@@ -22,7 +22,7 @@ class BasicBlock(nn.Module):
 
         if self.downsample is not None:
             identity = self.downsample(identity)
-        
+
         out += identity
         out = F.relu(out)
         return out
@@ -30,7 +30,7 @@ class BasicBlock(nn.Module):
 class BottleneckBlock(nn.Module):
     expansion = 4
 
-    def __init__(self, in_channels, out_channels, stride, downsample=None):
+    def __init__(self, in_channels, out_channels, stride=1, downsample=None):
         super(BottleneckBlock, self).__init__()
 
         self.conv1 = nn.Conv2d(in_channels, out_channels, kernel_size=1, stride=stride, padding=0)
@@ -40,7 +40,7 @@ class BottleneckBlock(nn.Module):
         self.conv3 = nn.Conv2d(out_channels, out_channels*self.expansion, kernel_size=1, padding=0)
         self.bn3 = nn.BatchNorm2d(out_channels*self.expansion)
 
-        self.downsample=None
+        self.downsample=downsample
 
     def forward(self, x):
         identity = x
@@ -51,27 +51,74 @@ class BottleneckBlock(nn.Module):
 
         if self.downsample is not None:
             identity = self.downsample(identity)
-        
+
         out += identity
         out = F.relu(out)
         return out
 
 class ResNet(nn.Module):
-    def __init__(self, num_classes):
-        pass
+    def __init__(self, config, num_classes):
+        super(ResNet, self).__init__()
+        self.block = config[0]
+        self.layers = config[1]
+        self.in_channels = 64
 
-    def forward():
-        pass
+        self.conv1 = nn.Conv2d(3, 64, kernel_size=7, stride=2, padding=3)
+        self.bn1 = nn.BatchNorm2d(64)
+        self.maxpool1 = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
 
-    def _make_layers():
-        pass
+        self.layer1 = self._make_layers(64, self.layers[0], stride=1)
+        self.layer2 = self._make_layers(128, self.layers[1], stride=2)
+        self.layer3 = self._make_layers(256, self.layers[2], stride=2)
+        self.layer4 = self._make_layers(512, self.layers[3], stride=2)
+
+        self.avgpool = nn.AdaptiveAvgPool2d((1, 1))
+        self.fc = nn.Linear(512*self.block.expansion, num_classes)
+
+    def forward(self, x):
+        out = self.conv1(x)
+        out = self.bn1(out)
+        out = F.relu(out)
+        out = self.maxpool1(out)
+
+        out = self.layer1(out)
+        out = self.layer2(out)
+        out = self.layer3(out)
+        out = self.layer4(out)
+
+        out = self.avgpool(out)
+        out = torch.flatten(out, 1)
+        out = self.fc(out)
+
+        return out
+
+    def _make_layers(self, out_channels, nb_blocks, stride=1):
+        layers = []
+        downsample = None
+
+        if self.in_channels != out_channels*self.block.expansion or stride != 1:
+            downsample = nn.Sequential(
+                nn.Conv2d(self.in_channels, out_channels*self.block.expansion, kernel_size=1, stride=stride),
+                nn.BatchNorm2d(out_channels*self.block.expansion)
+            )
+
+        layers.append(self.block(self.in_channels, out_channels, stride=stride, downsample=downsample))
+        self.in_channels=out_channels*self.block.expansion
+
+        for _ in range(1, nb_blocks):
+            layers.append(self.block(self.in_channels, out_channels))
+
+        return nn.Sequential(*layers)
 
 CONFIGS = {
     "resnet18": (BasicBlock, [2, 2, 2, 2]),
     "resnet34": (BasicBlock, [3, 4, 6, 3]),
+    "resnet50": (BottleneckBlock, [3, 4, 6, 3]),
+    "resnet101": (BottleneckBlock, [3, 4, 23, 3]),
+    "resnet152": (BottleneckBlock, [3, 8, 36, 3])
 }
 
-class VGGFactory:
+class ResNetFactory:
     @staticmethod
     def create_resnet(model_name, num_classes):
         if model_name not in CONFIGS:
