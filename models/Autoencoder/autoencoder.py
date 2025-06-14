3137f653d204ff2998333941a48ccb5118b06bf3
Le Hoang <lehoangnguyen510@gmail.com>
2025-06-15 00:27:26 +0200

    add ae + vae

---
+++ models/Autoencoder/autoencoder.py
@@ -0,0 +1,34 @@
+import torch
+import torch.nn as nn
+
+class Autoencoder(nn.Module):
+    def __init__(self):
+        super(Autoencoder, self).__init__()
+        self.encoder = nn.Sequential(
+            nn.Linear(28 * 28, 128),
+            nn.ReLU(),
+            nn.Linear(128, 64),
+            nn.ReLU(),
+            nn.Linear(64, 36),
+            nn.ReLU(),
+            nn.Linear(36, 18),
+            nn.ReLU(),
+            nn.Linear(18, 9)
+        )
+        self.decoder = nn.Sequential(
+            nn.Linear(9, 18),
+            nn.ReLU(),
+            nn.Linear(18, 36),
+            nn.ReLU(),
+            nn.Linear(36, 64),
+            nn.ReLU(),
+            nn.Linear(64, 128),
+            nn.ReLU(),
+            nn.Linear(128, 28 * 28),
+            nn.Sigmoid()
+        )
+
+    def forward(self, x):
+        out = self.encoder(x)
+        out = self.decoder(out)
+        return out
---
+++ models/VAE/vae.py
@@ -0,0 +1,41 @@
+import torch
+import torch.nn as nn
+
+class VAE(nn.Module):
+    def __init__(self, input_dim, hidden_dim, latent_dim):
+        super(VAE, self).__init__()
+        self.encoder = nn.Sequential(
+            nn.Linear(input_dim, hidden_dim),
+            nn.ReLU(),
+            nn.Linear(hidden_dim, latent_dim)
+        )
+
+        self.mean = nn.Linear(latent_dim, 2)
+        self.var = nn.Linear(latent_dim, 2)
+
+        self.decoder = nn.Sequential(
+            nn.Linear(2, latent_dim),
+            nn.ReLU(),
+            nn.Linear(latent_dim, hidden_dim),
+            nn.ReLU(),
+            nn.Linear(hidden_dim, input_dim),
+            nn.Sigmoid()
+        )
+
+    def encode(self, x):
+        out = self.encoder(x)
+        mean, var = self.mean(out), self.var(out)
+        return mean, var
+
+    def reparameterize(self, mean, var):
+        eps = torch.randn_like(var)
+        return mean + eps * var
+
+    def decode(self, z):
+        return self.decoder(z)
+
+    def forward(self, x):
+        mean, var = self.encode(x)
+        z = self.reparameterize(mean, var)
+        recon_x = self.decode(z)
+        return recon_x, mean, var
